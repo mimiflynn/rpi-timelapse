54b88fbd1bcd677248679cb1d56e2c124e472ab0
Mimi Flynn <mimiflynn@gmail.com>
2016-11-24 18:22:51 -0500

    flask integration

--- timelapse.py
+++ timelapse.py
@@ -10,10 +10,10 @@ camera.resolution = (3280, 2464)
 #camera.resolution = (3280, 2464)
 #camera.framerate = 15
 
-
-for i in range(10):
+def timelapse():
+  for i in range(10):
     sleep(5)
     camera.capture('/home/pi/Projects/Photos/images/image{0:04d}.jpg'.format(i))
 
-#system('convert -delay 10 -loop 0 images/image*.jpg images/animation.gif')
-print('done')
+  #system('convert -delay 10 -loop 0 images/image*.jpg images/animation.gif')
+  print('done')
