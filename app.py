74de4651ada32a6b6faca154d717b619856843e7
Mimi Flynn <mimiflynn@gmail.com>
2016-11-24 22:51:39 -0500

    add close to server trigger

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ def timelapse():
   #camera.resolution = (2592, 1944)
   #camera.resolution = (3280, 2464)
   #camera.framerate = 15
-  
+
   for i in range(10):
     sleep(5)
     camera.capture('/home/pi/Projects/Photos/images/image{0:04d}.jpg'.format(i))
 
+  camera.close()
   #system('convert -delay 10 -loop 0 images/image*.jpg images/animation.gif')
   print('done')
 
